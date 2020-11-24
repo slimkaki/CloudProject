68a9d503d1d9a1a1801a29cf808a3885b405deb7
slimkaki <rafaalmada98@gmail.com>
2020-11-24 16:24:43 -0300

    embelezando client

--- client.py
+++ client.py
@@ -19,7 +19,7 @@ def client(url, loop=True):
             "delete <id task>": "Deleta uma task especificada pelo id",
             "delete all": "Deleta todas as tasks",
             "update <id task>": "Atualiza uma task",
-            "url": "Inserir nova url da API",
+            "url": "Inserir nova url da API (Inserir a url com o '/tasks/')",
             "exit": "Sair deste client",
             "quit": "Sair deste client",
             "help": "Mostra os comandos possíveis"}
@@ -36,33 +36,43 @@ def client(url, loop=True):
             if (entrada.split()[1] == "all"):
                 get_all = url + "get_all"
                 response = requests.get(get_all)
-                print(response.json())
+                for task in response.json():
+                    print(f"\033[1m\033[96mTask {task['id']}:\033[0m")
+                    for item in task:
+                        if item == "id":
+                            continue
+                        print(f"\t\033[91m{item}:\033[0m {task[item]}")
             elif (entrada.split()[1].isdigit()):
                 get_id = url + f"get_single/{entrada.split()[1]}"
+                # print(f"get_id = {get_id}")
                 try:
                     response = requests.get(get_id)
-                    print(response.json())
+                    print(f"\033[1m\033[96mTask {response.json()['id']}:\033[0m")
+                    for item in response.json():
+                        if item == "id":
+                            continue
+                        print(f"\t\033[91m{item}:\033[0m {response.json()[item]}")
                 except:
-                    print("Essa task não existe!")
+                    print("\033[1m\033[91mEssa task não existe!\033[0m")
             else:
                 print(f"Comando '{entrada}' não identificado.")
 
         elif (entrada == "post"):
             print("Qual o título da nova task?")
-            title = input("tasks-api> ")
+            title = input("titulo> ")
             now = str(datetime.datetime.now())
             now_date = now.split(" ")[0]
             now_time = now.split(" ")[1].split(".")[0]
             pub_date = now_date + "T" + now_time + "Z"
             print("Qual a descrição da nova task?")
-            description = input("tasks-api> ")
+            description = input("descricao> ")
             body = {"title": title, "pub_date": pub_date, "description": description}
             post = url + "post"
             try:
                 response = requests.post(post, data=json.dumps(body))
                 print("Task criada com sucesso!")
             except:
-                print("Erro na criação da task!")
+                print("\033[1m\033[91mErro na criação da task!\033[0m")
         
         elif(entrada.split()[0] == "delete" and len(entrada.split()) > 1):
             if (entrada.split()[1] == "all"):
@@ -71,14 +81,14 @@ def client(url, loop=True):
                     delete = url + "delete_all"
                     response = requests.delete(delete)
                 except:
-                    print("Não foi possivel deletar todas as tasks!")
+                    print("\033[1m\033[91mNão foi possivel deletar todas as tasks!\033[0m")
             elif (entrada.split()[1].isdigit()):
                 print(f"Deletando task {entrada.split()[1]}")
                 try:
                     delete = url + "delete/" + str(entrada.split()[1])
                     response = requests.delete(delete)
                 except:
-                    print(f"Não foi possível deletar a task {entrada.split()[1]}!")
+                    print(f"\033[1m\033[91mNão foi possível deletar a task {entrada.split()[1]}!\033[0m")
             else:
                 print(f"Comando '{entrada}' não identificado.")
         
@@ -99,12 +109,12 @@ def client(url, loop=True):
                     response = requests.put(my_update, data=json.dumps(body))
                     print(f"Task {entrada.split()[1]} alterada com sucesso!")
                 except:
-                    print("Não foi possível alterar a task em questão!")
+                    print("\033[1m\033[91mNão foi possível alterar a task em questão!\033[0m")
             else:
-                print(f"Comando '{entrada}' não identificado.")
+                print(f"\033[1m\033[91mComando '{entrada}' não identificado.\033[0m")
         elif (entrada == "url"):
             print("Insira a nova url:")
-            url = input("tasks-api> ")
+            url = input("nova-url> ")
             # url = url + "/tasks/"
 
         elif (entrada == "quit" or entrada == "exit"):
@@ -119,5 +129,5 @@ def client(url, loop=True):
             print(f"Comando '{entrada}' não identificado.")
             
 if __name__ == '__main__':
-    url = "rafa-load-balancer-331215668.us-east-1.elb.amazonaws.com/" + "tasks/"
+    url = "http://rafa-load-balancer-1231070759.us-east-1.elb.amazonaws.com/" + "tasks/"
     client(url)
